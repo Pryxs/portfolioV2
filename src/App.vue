46e5d52403f2b5464021ff91d3dd778d29da1ca7
Pryxs <corentin25200@gmail.com>
2022-09-23 15:30:46 +0200

    feat(close button animation)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import { RouterView } from 'vue-router'
 import { ref } from "vue"
+import RobotError from '@/assets/robot-error.svg?component'
 
 export default {
   components: {
     RouterView,
+    RobotError
   },
 
   data: function () {
@@ -27,9 +29,46 @@ export default {
 
 <template>
   <RouterView v-if="windowWidth > 785"/>
-  <div v-else>nop</div>
+  <div v-else class="advice">
+    <span class="advice_title">Oooops</span>
+    <p class="advice_message">Vous devriez essayer avec un écran plus <strong>grand</strong> !</p>
+    <div class="advice_image">
+      <RobotError/>
+    </div>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+  .advice{
+    background-color: #222;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1rem;
+    font-family: 'Cabin', sans-serif;
+    padding: 1rem;
+    text-align: center;
 
+    &_title{
+      font-size: 40px;
+    }
+
+    &_message{
+      font-size: 24px;
+
+      strong{
+        color: $primary-color;
+      }
+    }
+
+    &_image{
+      margin-top: 4rem;
+
+      svg{
+        transform: scale(1.6);
+      }
+    }
+  }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,7 +6,7 @@
             <div class="modal_content" @wheel.prevent="horizontalScroll($event)">
                 <slot></slot>
             </div>
-            <button class="modal_close" @click="closeModal">
+            <button id="test" class="modal_close" @click="closeModal">
                 <span class="modal_close_circle"></span>
                 <span class="modal_close_bar -left"></span>
                 <span class="modal_close_bar -right"></span>
@@ -17,6 +17,8 @@
 
 <script>
     import { defineComponent, ref } from 'vue';
+    import gsap from "gsap";
+import { routeLocationKey } from 'vue-router';
 
     export default defineComponent({
         setup() {
@@ -35,6 +37,13 @@
                 showModal,
                 closeModal,
             };
+        },
+
+        updated(){
+            gsap.to('.modal_close_bar',0,{height: 40, ease: "power2.easeIn",})
+            gsap.to('.-right', {rotation: -45, delay: .1, ease: "power2.easeInOut"})
+            gsap.to('.-left', {rotation: 45, delay: .1, ease: "power2.easeInOut"})
+            gsap.to('.modal_close_circle', {opacity: 1, delay: .2, duration: .6, ease: "power2.easeIn"})
         },
 
         methods:{
@@ -91,12 +100,13 @@
                 height: 64px;
                 width: 64px;
                 transition: .4s ease-in-out;
+                opacity: 0;
             }
 
             &_bar{
                 width: 1px;
                 background-color: $text-color;
-                height: 40px;
+                height: 0;
                 transform-origin: center;
                 position: absolute;
                 top: 50%;
@@ -104,11 +114,11 @@
                 transition: .4s ease-in-out;
 
                 &.-left{
-                    transform: translate(-50%, -50%) rotate(45deg);
+                    transform: translate(-50%, -50%) rotate(90deg);
                 }
 
                 &.-right{
-                    transform: translate(-50%, -50%) rotate(-45deg);
+                    transform: translate(-50%, -50%) rotate(-90deg);
                 }
             }
 
